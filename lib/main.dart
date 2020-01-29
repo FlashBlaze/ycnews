c13c840b1037725f9caffc43482b898c4f4e5fc0
FlashBlaze <neerajlagwankar@gmail.com>
2020-01-29 21:34:07 +0530

    Fetching 5 records from the API

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert' as convert;
 
 void main() => runApp(YCNews());
 
@@ -9,8 +11,42 @@ class YCNews extends StatelessWidget {
         title: 'YCNews',
         home: Scaffold(
           body: Center(
-            child: Text('Placeholder'),
+            child: PostData(),
           ),
         ));
+  }
+}
+
+class PostData extends StatefulWidget {
+  @override
+  _PostDataState createState() => _PostDataState();
+}
+
+class _PostDataState extends State<PostData> {
+  void getPosts() async {
+    var uri =
+        'https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty&orderBy="\$key"&limitToFirst=5';
+    var response = await http.get(uri);
+    List ids = convert.jsonDecode(response.body);
+
+    for (var i = 0; i < ids.length; i++) {
+      var postUri =
+          'https://hacker-news.firebaseio.com/v0/item/${ids[i]}.json?print=pretty';
+      var postResponse = await http.get(postUri);
+      print(convert.jsonDecode(postResponse.body)['title']);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(
+          child: RaisedButton(
+        onPressed: () {
+          getPosts();
+        },
+        child: Text('Get 5 post titles'),
+      )),
+    );
   }
 }
