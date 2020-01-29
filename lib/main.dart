723564f9e719040279b87e0693ae5357bcfc5630
FlashBlaze <neerajlagwankar@gmail.com>
2020-01-29 11:36:43 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(YCNews());
+
+class YCNews extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'YCNews',
+        home: Scaffold(
+          body: Center(
+            child: Text('Placeholder'),
+          ),
+        ));
+  }
+}
